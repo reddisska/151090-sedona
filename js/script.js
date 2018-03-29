c44460a910341b13f450802dfc819e51eae25874
reddisska <36864034+reddisska@users.noreply.github.com>
2018-03-29 10:46:52 +0300

    Оживлена форма поиска

---
+++ js/script.js
@@ -0,0 +1,6 @@
+	var hotelSearch = document.querySelector(".button-search");
+		var popup = document.querySelector(".search-form");
+		hotelSearch.addEventListener("click", function(evt) {
+			evt.preventDefault();
+			popup.classList.toggle("form-hidden");
+		}, false);
